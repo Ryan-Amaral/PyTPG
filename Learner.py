120c8c65648cb84fe66484f4a62057057e13c1d6
Ryan Amaral <ryan_amaral@live.com>
2018-06-29 16:49:32 -0300

    Starting training initialization method

---
+++ Learner.py
@@ -0,0 +1,5 @@
+"""
+A Learner.
+"""
+class Leaner:
+    
---
+++ TpgTrainer.py
@@ -0,0 +1,86 @@
+"""
+The main class to do training on a population of Teams.
+Created By: Ryan Amaral.
+Created On: June 29, 2018 13:44.
+Created Because: I was tired of Java.
+"""
+class TpgTrainer:
+
+    import random
+    import time
+    import pickle
+
+    """
+    Initializes the Training procedure, potentially picking up from a
+    previously left off point.
+    Args:
+        actions        : List of longs, the actions available in the env.
+        randSeed       :
+        teamPopSizeInit: Initial Team population size.
+        gap            : Proportion of agents to replace per gen.
+        pLearnerDelete :
+        pLearnerAdd    :
+        pMutateAction  :
+        pActionIsTeam  :
+        maxTeamSize    :
+        maxProgramSize :
+        pProgramDelete :
+        pProgramAdd    :
+        pProgramSwap   :
+        pProgramMutate :
+        popInit        : Object containing all info needed to carry on from a
+            previous training session. Serialize and deserialize with pickle.
+    """
+    def __init__(self, actions, randSeed=0, teamPopSizeInit=360, gap=0.5,
+            pLearnerDelete=0.7, pLearnerAdd=0.7, pMutateAction=0.2,
+            pActionIsTeam=0.5, maxTeamSize=5, maxProgramSize=96,
+            pProgramDelete=0.5, pProgramAdd=0.5, pProgramSwap=1.0,
+            pProgramMutate=1.0, popInit=None):
+
+        # set the variables
+        self.actions = actions
+        self.teamPopSizeInit = teamPopSizeInit
+        self.gap = gap
+        self.pLearnerDelete = pLearnerDelete
+        self.pLearnerAdd = pLearnerAdd
+        self.pMutateAction = pMutateAction
+        self.pActionIsTeam = pActionIsTeam
+        self.maxTeamSize = maxTeamSize
+        self.maxProgramSize = maxProgramSize
+        self.pProgramDelete = pProgramDelete
+        self.pProgramAdd = pProgramAdd
+        self.pProgramSwap = pProgramSwap
+        self.pProgramMutate = pProgramMutate
+
+        # establish random for training
+        if randSeed == 0:
+            random.seed(int(round(time.time())))
+        else:
+            random.seed(randSeed)
+
+        # create initial populations if starting anew
+        if popInit is None:
+            self.teams = None
+            self.rootTeams = None
+            self.learners = None
+            self.curGen = 0
+            initPops()
+        else:
+            self.teams = popInit.teams
+            self.rootTeams = popInit.rootTeams
+            self.learners = popInit.learners
+            self.curGen = popInit.gen
+
+    """
+    Creates the initial population of teams and learners, on initialization of
+    training.
+    """
+    def initPops(self):
+        # create teams to fill population
+        for i in range(self.teamPopSizeInit):
+            # take two distinct initial actions for each of two learners on team
+            ac1 = self.actions[random.randint(0,len(self.actions))]
+            tmpActions = [a for a in self.actions if a != ac1]
+            ac2 = tmpActions[random.randint(0,len(tmpActions))]
+
+            team = Team()
