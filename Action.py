37d8a124e97d45f9e07cbd245f9a36295dfdb7f6
Ryan Amaral <ryan_amaral@live.com>
2018-07-18 12:07:58 -0300

    implemented actomic check for actions

--- Action.py
+++ Action.py
@@ -3,12 +3,18 @@ An action.
 """
 class Action:
 
-    def __init__(self):
-        pass
+    """
+    Creates an action object with a specified action, either a team or atomic.
+    Args:
+        action:
+            (Long or Team) Long if atomic, Team otherwise.
+    """
+    def __init__(self, action):
+        self.action = action
 
     """
     Returns a boolean telling whether this action is a specific action (atomic),
     or refers to a team (not atomic).
     """
     def isAtomic():
-        pass
+        return type(action) is not Team
