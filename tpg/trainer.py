a9a9ebde485d128debb931bcb38246f3c5d0614f
Ryan-Amaral <ryan_amaral@live.com>
2019-07-31 16:10:36 -0300

    remove unesessary removal of elites

--- tpg/trainer.py
+++ tpg/trainer.py
@@ -217,8 +217,6 @@ class Trainer:
             if team.numLearnersReferencing == 0 or team in self.elites:
                 self.rootTeams.append(team)
 
-        self.elites = []
-
     def countRootTeams(self):
         numRTeams = 0
         for team in self.teams:
