38db4d448e4b07b305abc53a0eca6768fa400c81
ryan-amaral <ryan_amaral@live.com>
2020-11-26 17:47:22 -0400

    update setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='PyTPG',
-    version='0.95',
+    version='0.96',
     packages=['tpg', 'tpg.configuration'],
     install_requires=['numba','numpy', 'unittest-xml-reporting'],
     license='MIT',
