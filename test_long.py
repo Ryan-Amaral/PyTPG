c7d6bb0abb02276c7e15b65a12da81211ddeb4fd
ryan-amaral <ryan_amaral@live.com>
2020-11-26 17:26:47 -0400

    remove debug stuff from trainer

--- test_long.py
+++ test_long.py
@@ -3,7 +3,7 @@ from tpg.agent import loadAgent
 from tpg.trainer import loadTrainer
 
 _, mpScore = runPopulationParallel(
-    envName="Boxing-v0", gens=1500, popSize=200, reps=1,
-    frames=1000, processes=20, nRandFrames=30)
+    envName="Boxing-v0", gens=1500, popSize=360, reps=1,
+    frames=18000, processes=20, nRandFrames=30)
 
 print(f"MP Score: {mpScore}")
