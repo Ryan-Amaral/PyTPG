c3d4e8ddbba3316fb794980319f0ebd6975347fc
ryan-amaral <ryan_amaral@live.com>
2021-05-11 12:17:50 -0300

    fix register size on action programs

--- tpg/configuration/conf_action_object.py
+++ tpg/configuration/conf_action_object.py
@@ -97,7 +97,7 @@ class ConfActionObject:
                 '''
                 print("Index error")
 
-        self.registers = np.zeros(initParams["nActRegisters"])
+        self.registers = np.zeros(max(initParams["nActRegisters"], initParams["nDestinations"]))
 
     """
     Returns the action code, and if applicable corresponding real action.
